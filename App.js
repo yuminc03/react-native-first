53f42f27192be6adddb18492b65562b642f2eca6
Yumin Chu <ym.chu@lifesemantics.kr>
2024-02-28 14:55:52 +0900

    Feat: Text, 배경색 바꿔보기

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#B399E0',
     alignItems: 'center',
     justifyContent: 'center',
   },
