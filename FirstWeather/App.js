dc070db4b4b21e34b7dd6cbc5aae9042599e0e6f
Yumin Chu <yuminc03@gmail.com>
2024-03-03 01:06:31 +0900

    Feat: 화면 크기 구하기, ScrollView 넣기, scrollView style 바꿈

--- FirstWeather/App.js
+++ FirstWeather/App.js
@@ -1,4 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
@@ -6,7 +8,12 @@ export default function App() {
       <View style = {styles.city}>
         <Text style = {styles.cityName}>Seoul</Text>
       </View>
-      <View style = {styles.weather}>
+      <ScrollView 
+      pagingEnabled 
+      horizontal
+      showsHorizontalScrollIndicator = {false}
+      contentContainerStyle = {styles.weather}
+      >
         <View style = {styles.day}>
           <Text style = {styles.temp}>27</Text>
           <Text style = {styles.description}>Sunny</Text>
@@ -19,7 +26,7 @@ export default function App() {
           <Text style = {styles.temp}>27</Text>
           <Text style = {styles.description}>Sunny</Text>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -39,11 +46,10 @@ const styles = StyleSheet.create({
     fontWeight: '500'
   },
   weather: {
-    flex: 3,
   },
   day: {
-    flex: 1,
-    alignItems: 'center',
+    width: SCREEN_WIDTH,
+    alignItems: 'center'
   },
   temp: {
     marginTop: 50,
