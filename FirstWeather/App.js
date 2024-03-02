bd01d8de025af956355a8fe1b574742aec605664
Yumin Chu <yuminc03@gmail.com>
2024-03-02 20:15:23 +0900

    Feat: Weather city, Temperature, description UI

--- FirstWeather/App.js
+++ FirstWeather/App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style = {styles.city}>
+        <Text style = {styles.cityName}>Seoul</Text>
+      </View>
+      <View style = {styles.weather}>
+        <View style = {styles.day}>
+          <Text style = {styles.temp}>27</Text>
+          <Text style = {styles.description}>Sunny</Text>
+        </View>
+        <View style = {styles.day}>
+          <Text style = {styles.temp}>27</Text>
+          <Text style = {styles.description}>Sunny</Text>
+        </View>
+        <View style = {styles.day}>
+          <Text style = {styles.temp}>27</Text>
+          <Text style = {styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#9C6DEA'
   },
-});
+  city: {
+    flex: 1.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500'
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  }
+})
