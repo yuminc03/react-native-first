1318acdf0db55fd6575d33a197d2cd6beb1ce85a
Yumin Chu <yuminc03@gmail.com>
2024-03-03 20:11:57 +0900

    Feat: expo location을 사용한 현재 위치 구하기, LoadingView 표시

--- FirstWeather/App.js
+++ FirstWeather/App.js
@@ -1,12 +1,41 @@
-import { ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import * as Location from 'expo-location';
 
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const API_KEY = 'a4a7614309bb0c73eeaa7b6ef3b457f4'
 
 export default function App() {
+  const [city, setCity] = useState('loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+    const {granted} = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: {latitude, longitude},
+    } = await Location.getCurrentPositionAsync({accuracy: 5});
+    const location = await Location.reverseGeocodeAsync(
+      {latitude, longitude}, 
+      {useGoogleMaps: false}
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
-    <View style={styles.container}>
+    <View style = {styles.container}>
       <View style = {styles.city}>
-        <Text style = {styles.cityName}>Seoul</Text>
+        <Text style = {styles.cityName}>{city}</Text>
       </View>
       <ScrollView 
       pagingEnabled 
@@ -14,18 +43,19 @@ export default function App() {
       showsHorizontalScrollIndicator = {false}
       contentContainerStyle = {styles.weather}
       >
-        <View style = {styles.day}>
-          <Text style = {styles.temp}>27</Text>
-          <Text style = {styles.description}>Sunny</Text>
-        </View>
-        <View style = {styles.day}>
-          <Text style = {styles.temp}>27</Text>
-          <Text style = {styles.description}>Sunny</Text>
-        </View>
-        <View style = {styles.day}>
-          <Text style = {styles.temp}>27</Text>
-          <Text style = {styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style = {styles.day}>
+            <ActivityIndicator
+              color = 'white'
+              style = {{merginTop: 10}}
+              size = {'large'}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => 
+          <View key = {index} style = {styles.day}></View>
+          )
+        )}
       </ScrollView>
     </View>
   );
