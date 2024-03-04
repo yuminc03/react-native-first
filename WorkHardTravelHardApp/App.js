ad5881d972411e33c4c2249420fa8b038252e563
Yumin Chu <ym.chu@lifesemantics.kr>
2024-03-04 13:59:16 +0900

    Feat: TextInput 속성 살피기

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,38 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   StyleSheet, 
-  Text, 
+  Text,
+  TextInput, 
   View, 
-  TouchableOpacity, 
-  TouchableHighlight, 
-  TouchableWithoutFeedback, 
-  Pressable 
+  TouchableOpacity
 } from 'react-native';
 import { theme } from './colors';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => console.log(payload);
+
   return (
     <View style = {styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style = "auto" />
       <View style = {styles.header}>
-        {/* <TouchableHighlight 
-        underlayColor = 'red'
-        activeOpacity = {0.5}
-        onPress = {() => console.log('pressed')}
-        >
-          <Text style = {styles.btnText}>Travel</Text>
-        </TouchableHighlight> */}
-        <TouchableOpacity>
-          <Text style = {styles.btnText}>Work</Text>
+        <TouchableOpacity onPress = {work}>
+          <Text style = {{... styles.btnText, color: working ? 'white' : theme.gray}}>
+            Work
+          </Text>
         </TouchableOpacity>
-        {/* <Pressable>
-          <Text style = {styles.btnText}>Work</Text>
-        </Pressable> */}
-        <TouchableWithoutFeedback onPress = {() => console.log('pressed')}>
-          <Text style = {styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress = {travel}>
+          <Text style = {{... styles.btnText, color: !working ? 'white' : theme.gray}}>
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onChangeText = {onChangeText}
+          placeholder = {working ? 'Add a To Do' : 'Where do you want to go?'} 
+          style = {styles.input}
+        />
       </View>
     </View>
   );
@@ -52,6 +56,14 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 44,
     fontWeight: '600',
-    color: 'white'
+    color: 'white',
   },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  }
 });
