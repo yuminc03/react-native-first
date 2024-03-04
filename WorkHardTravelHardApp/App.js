626345ac5fa1d54ac7f3aadc56ad5a78d4df54a9
Yumin Chu <ym.chu@lifesemantics.kr>
2024-03-04 11:35:31 +0900

    Feat: TouchableHightLight, TouchableOpacity, Pressable, TouchableWithoutFeedback 사용

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity, 
+  TouchableHighlight, 
+  TouchableWithoutFeedback, 
+  Pressable 
+} from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style = {styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style = "auto" />
+      <View style = {styles.header}>
+        {/* <TouchableHighlight 
+        underlayColor = 'red'
+        activeOpacity = {0.5}
+        onPress = {() => console.log('pressed')}
+        >
+          <Text style = {styles.btnText}>Travel</Text>
+        </TouchableHighlight> */}
+        <TouchableOpacity>
+          <Text style = {styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        {/* <Pressable>
+          <Text style = {styles.btnText}>Work</Text>
+        </Pressable> */}
+        <TouchableWithoutFeedback onPress = {() => console.log('pressed')}>
+          <Text style = {styles.btnText}>Travel</Text>
+        </TouchableWithoutFeedback>
+      </View>
     </View>
   );
 }
@@ -13,8 +41,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 44,
+    fontWeight: '600',
+    color: 'white'
   },
 });
