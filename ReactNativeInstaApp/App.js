e93886850638ae586f0cc02c4cec94d111739ba8
Yumin Chu <yuminc03@gmail.com>
2024-03-11 15:33:38 +0900

    Feat: tabBarItem 아이콘 넣기

--- ReactNativeInstaApp/App.js
+++ ReactNativeInstaApp/App.js
@@ -1,11 +1,13 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionic from 'react-native-vector-icons/Ionicons';
 import React from 'react';
 import Home from './src/screens/Home';
 import Search from './src/screens/Search';
 import Activity from './src/screens/Activity';
 import Profile from './src/screens/Profile';
-import { NavigationContainer } from '@react-navigation/native';
 import Status from './src/screens/Status';
 import FriendProfile from './src/screens/FriendProfile';
 import EditProfile from './src/screens/EditProfile';
@@ -15,14 +17,29 @@ const App = () => {
   const Tab = createBottomTabNavigator();
   const BottomTabScreen = () => {
     return (
-      <Tab.Navigator 
-        screenOptions = {() => ({
+      <Tab.Navigator
+        screenOptions = {({ route }) => ({
           tabBarHideOnKeyboard: true,
           tabBarShowLabel: false,
           headerShown: false,
           tabBarStyle: {
             height: 70,
-          }
+          },
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName;
+            color = 'black';
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search' : 'ios-search-outline';
+            } else if (route.name === 'Activity') {
+              iconName = focused ? 'ios-heart' : 'ios-heart-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-person-circle' : 'ios-person-outline';
+            }
+
+            return <Ionic name={iconName} size={size} color={color}/>;
+          },
         })}
       >
         <Tab.Screen name = "Home" component = {Home}/>
@@ -35,7 +52,7 @@ const App = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions = {{headerShown: false}}>
+      <Stack.Navigator screenOptions = {{ headerShown: false }}>
         <Stack.Screen name = "Bottom" component = {BottomTabScreen}/>
         <Stack.Screen name = "Status" component = {Status}/>
         <Stack.Screen name = "FriendProfile" component = {FriendProfile}/>
