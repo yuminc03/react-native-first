431250f12408a0d4278485c691d7a1e6a31384f4
Yumin Chu <yuminc03@gmail.com>
2024-03-12 14:01:26 +0900

    Feat: 스토리 Status UI 만들기

--- RNInstagramApp/src/screens/Status.js
+++ RNInstagramApp/src/screens/Status.js
@@ -1,11 +1,86 @@
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar, Image, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
-const Status = () => {
+const Status = ({route, navigation}) => {
+  const {name, image} = route.params;
   return (
-    <View>
-      <Text>Status</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        backgroundColor: 'black',
+        height: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      <StatusBar backgroundColor="black" barStyle="light-content"/>
+      <View
+        style={{
+          height: 3,
+          width: '95%',
+          borderWidth: 1,
+          backgroundColor: 'gray',
+          position: 'absolute',
+          top: 18,
+        }}
+      />
+      <View
+        style={{
+          padding: 15,
+          flexDirection: 'row',
+          alignItems: 'center',
+          position: 'absolute',
+          top: 12,
+          left: 0,
+          width: '90%',
+        }}
+      >
+        <View
+          style={{
+            width: 30,
+            height: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            source={image}
+            style={{
+              borderRadius: 100,
+              backgroundColor: 'orange',
+              width: '92%',
+              height: '92%',
+              resizeMode: 'cover',
+            }}
+          />
+        </View>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            width: '100%',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 15, paddingLeft: 10 }}>
+            {name}
+          </Text>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionic
+              name="close"
+              style={{ color: 'white', fontSize: 15, opacity: 0.6 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Image
+        source={image}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: 500,
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
