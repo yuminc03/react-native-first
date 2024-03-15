bafd383fc0c13ac418f907b28337c887376adafc
Yumin Chu <yuminc03@gmail.com>
2024-03-15 11:15:01 +0900

    Feat: Post Action, Comments UI 작업

--- RNInstagramApp/src/components/PostItem.js
+++ RNInstagramApp/src/components/PostItem.js
@@ -1,7 +1,8 @@
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import Feather from 'react-native-vector-icons/Feather';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
 const PostItem = ({ data }) => {
   const [like, setLike] = useState(data.isLike);
@@ -50,7 +51,12 @@ const PostItem = ({ data }) => {
           paddingVertical: 15,
         }}
       >
-        <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
           <TouchableOpacity onPress={() => setLike(prev => !prev)}>
             <AntDesign
               name={like ? 'heart' : 'hearto'}
@@ -62,10 +68,74 @@ const PostItem = ({ data }) => {
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => setLike(prev => !prev)}>
-            
+            <Ionic
+              name="chatbubble-outline"
+              style={{
+                fontSize: 20,
+                paddingRight: 10,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Feather name="navigation" style={{ fontSize: 20 }}/>
           </TouchableOpacity>
         </View>
         <Feather name="bookmark" style={{ fontSize: 20 }}/>
+      </View>
+      <View style={{ paddingHorizontal: 15 }}>
+        <Text>
+          좋아요 {like ? data.likes + 1 : data.likes} 개
+        </Text>
+        <Text
+          style={{
+            fontWeight: '700',
+            fontSize: 14,
+            paddingVertical: 2,
+          }}
+        >
+          게시글 설명글입니다.
+        </Text>
+        <Text
+          style={{
+            opacity: 0.4,
+            paddingVertical: 2,
+            marginTop: 5,
+            marginBottom: 5,
+          }}
+        >
+          댓글 모두 보기
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              source={data.postPersonImage}
+              style={{
+                width: 25,
+                height: 25,
+                borderRadius: 100,
+                backgroundColor: 'orange',
+                marginRight: 10,
+              }}
+            />
+            <TextInput
+              placeholder="댓글 달기..."
+              style={{ opacity: 0.5 }}
+            />
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={{ color: '#0095F6' }}>게시</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
