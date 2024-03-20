647c2d2b8b11723993d78f94624e54c6f2eed38d
Yumin Chu <yuminc03@gmail.com>
2024-03-20 14:37:55 +0900

    Feat: Activity Screen UI - 필요한 파일 생성

---
+++ RNInstagramApp/src/components/ActivityRecommend.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ActivityRecommend = () => {
+  return (
+    <View>
+      <Text>ActivityRecommend</Text>
+    </View>
+  );
+};
+
+export default ActivityRecommend;
